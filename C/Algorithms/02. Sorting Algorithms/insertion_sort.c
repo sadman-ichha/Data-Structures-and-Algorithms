1459e25dc86e3ea4cd56a4d91539fc8138ae360a
sadman_ichha <sadmanichha1@gmail.com>
2023-12-21 22:28:14 +0600

    array problem

---
+++ C/Algorithms/02. Sorting Algorithms/insertion_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void printArray(int arr[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {4, 2, 12, 5, 14, 8};
+    int check_value, i, temp_empty;
+    int length = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: \n");
+    printArray(arr, length);
+
+    for (i = 1; i < length; i++)
+    {
+        check_value = arr[i];
+        temp_empty = i - 1;
+        while (temp_empty >= 0 && arr[temp_empty] > check_value)
+        {
+            arr[temp_empty + 1] = arr[temp_empty];
+            // temp_empty = temp_empty - 1;
+            temp_empty--;
+        }
+        arr[temp_empty + 1] = check_value;
+    }
+
+    printf("Sorted array: \n");
+    printArray(arr, length);
+
+    return 0;
+}
--- C/Data-Structures/01. Complexity/01_time_complexity.c
+++ C/Data-Structures/01. Complexity/01_time_complexity.c
@@ -2,6 +2,10 @@
 
 int main()
 {
-    
+    int x = 100;
+    printf("x = %d\n", x);
+    int y = --x;
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
     return 0;
 }
