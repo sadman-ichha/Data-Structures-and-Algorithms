c3e8a1f9ade1fb88bd9492a966c4151ff5e03c56
sadman_ichha <sadmanichha1@gmail.com>
2024-01-08 13:12:22 +0600

    doubly linked list insertion, remove node, reverse

--- C/Data-Structures/03. LinkedList/05_doubly_linked_list.c
+++ C/Data-Structures/03. LinkedList/05_doubly_linked_list.c
@@ -1,45 +1,148 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Node structure for a doubly linked list
 struct node {
-    int data;
-    struct node *next;
-    struct node *prev;
+    struct node *prev; // Pointer to the previous node
+    int data; // Data stored in the node
+    struct node *next; // Pointer to the next node
 };
 
-typedef struct node Node;
+typedef struct node Node; // Typedef for simplicity
 
-Node *createNode(int data, Node *next, Node *prev);
-void printLinkedList(Node *head);
+// Function declarations
+Node *createNode(int data); // Creates a new node
+void printLinkedList(Node *head); // Prints the linked list
+void insertAtBegining(Node **head, int data); // Inserts a node at the beginning
+void insertAtEnd(Node **head, int data); // Inserts a node at the end
+void removeNode(Node **head, int data); // Removes a node by value
+void reverseList(Node **head); // Reverses the linked list
 
+// Main function
 int main() {
-    // Creating nodes and linking them to form a linked list
-    Node *head = createNode(10, NULL, NULL);
-    head->next = createNode(20, NULL, head);
-    head->next->next = createNode(30, NULL, head->next);
+    Node *head = NULL; // Initializing an empty linked list
 
-    // Printing the linked list
+    // Inserting nodes at the beginning and end of the list
+    insertAtBegining(&head, 10);
+    insertAtBegining(&head, 5);
+    insertAtEnd(&head, 15);
+    insertAtEnd(&head, 20);
+    insertAtEnd(&head, 25);
+
+
+
+    // Printing the original list
     printf("Linked List: ");
+    printLinkedList(head);
+
+    // Removing a node and printing the updated list
+    int removeValue = 5;
+    removeNode(&head, removeValue);
+    printf("List after removing %d: ", removeValue);
+    printLinkedList(head);
+
+    // Reversing the list
+    reverseList(&head);
+
+    // Displaying the reversed list
+    printf("Reversed List: ");
     printLinkedList(head);
 
     return 0;
 }
 
-Node *createNode(int data, Node *next, Node *prev) {
-    Node *new_node = (Node *)malloc(sizeof(Node));
-    if (new_node == NULL) {
-        printf("Error! Could Not Allocate The memory\n");
-        exit(1);
+// Function to reverse the doubly linked list
+void reverseList(Node **head) {
+    Node *temp = NULL, *current = *head;
+
+    while (current != NULL) {
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+        current = current->prev;
     }
 
+    if (temp != NULL) {
+        *head = temp->prev;
+    }
+}
+
+// Function to remove a node by value
+void removeNode(Node **head, int data) {
+    if (*head == NULL) {
+        printf("Linked List is empty!\n");
+        return;
+    }
+
+    Node *temp = *head;
+    while (temp != NULL && temp->data != data) {
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        printf("Node not found!\n");
+        return;
+    }
+
+    if (temp->prev == NULL) {
+        *head = temp->next;
+    } else {
+        temp->prev->next = temp->next;
+    }
+
+    if (temp->next != NULL) {
+        temp->next->prev = temp->prev;
+    }
+    free(temp);
+}
+
+// Function to insert a node at the end of the list
+void insertAtEnd(Node **head, int data) {
+    Node *newNode = createNode(data);
+    if (*head == NULL) {
+        *head = createNode(data);
+    } else {
+        Node *temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->prev = temp;
+    }
+}
+
+// Function to insert a node at the beginning of the list
+void insertAtBegining(Node **head, int data) {
+    Node *newNode = createNode(data);
+    if (*head == NULL) {
+        *head = createNode(data);
+    } else {
+        newNode->next = *head;
+        (*head)->prev = newNode;
+        *head = newNode;
+    }
+}
+
+// Function to create a new node
+Node *createNode(int data) {
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    if (new_node == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    new_node->prev = NULL;
     new_node->data = data;
-    new_node->prev = prev;
-    new_node->next = next;
+    new_node->next = NULL;
     return new_node;
 }
 
+// Function to print the linked list
 void printLinkedList(Node *head) {
     Node *current = head;
+
+    if (head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
     while (current != NULL) {
         printf("%d -> ", current->data);
         current = current->next;
--- C/Data-Structures/03. LinkedList/tempCodeRunnerFile.c
+++ C/Data-Structures/03. LinkedList/tempCodeRunnerFile.c
@@ -1,1 +1,1 @@
-10, NULL, NULL
+head
