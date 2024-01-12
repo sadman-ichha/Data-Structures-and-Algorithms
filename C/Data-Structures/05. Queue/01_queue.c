4c8ee311e9d51f43ee1f4f04307fb31c8de24843
sadman_ichha <sadmanichha1@gmail.com>
2024-01-12 22:43:26 +0600

    Queue: Dequeue,enqueue

--- C/Data-Structures/05. Queue/01_queue.c
+++ C/Data-Structures/05. Queue/01_queue.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
-
+#include <stdbool.h>
+#define MAXQUEUE 5
+int queue[MAXQUEUE];
+int front =0, rear =-1,totalData =0;
+void enqueue(int data);
+int dequeue();
 int main()
 {
-    printf("sadman");
+    
+    return 0;
+}
+
+bool isFull(){
+    if (totalData == MAXQUEUE)
+    {
+        /* code */
+    }
+    
+}
+void enqueue(int data){
+   if (rear== MAXQUEUE)
+   {
+    /* code */
+   }
+   
+}
+
+int dequeue(){
     return 0;
 }
